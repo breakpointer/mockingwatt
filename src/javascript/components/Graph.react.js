504c86e114cd9dfd69f8bf4cf55c25c05b770e12
Brendan Irvine-Broque <brendanib@gmail.com>
2014-11-18 15:07:09 -0800

    graph looks pretty

--- src/javascript/components/Graph.react.js
+++ src/javascript/components/Graph.react.js
@@ -6,50 +6,88 @@ var Graph = React.createClass({
 
   render: function() {
 
-  	var chartData = [];
-    var baselineData = [];
+  	var redData = [];
+    var greenData = [];
+    var yellowData = [];
 
-  	if (this.props.usageData.length > 3) {
-	  	for (var i = 0; i < this.props.usageData.length; i++) {
+  	for (var i = 0; i < this.props.usageData.length; i++) {
 
-        // Subtract the bias from the value
-        var biasedData = this.props.usageData[i].value - this.props.usageData[i].bias
+      // Subtract the bias from the value
+      if (this.props.usageData[i].bias > 0) {
+        var biasedData = this.props.usageData[i].value + this.props.usageData[i].bias
+        redData.push([this.props.usageData[i].slot, biasedData])
+      } else {
+        redData.push([this.props.usageData[i].slot, this.props.usageData[i].value])
+      }
 
-        chartData.push([this.props.usageData[i].slot, biasedData])
 
-        // Subtract the 
-	  		baselineData.push([this.props.usageData[i].slot, this.props.usageData[i].value])
-	  	}
-  	} else {
-      // This is just a hack to prevent Highcharts from barfing when it gets values that don't exist yet.
-  		chartData = [[31, 32], [32, 33]]
-      baselineData = [[31, 32], [32, 33]]
+  		greenData.push([this.props.usageData[i].slot, this.props.usageData[i].value])
+
+      if (this.props.usageData[i].bias < 0) {
+        var foo = this.props.usageData[i].value + this.props.usageData[i].bias
+        yellowData.push([this.props.usageData[i].slot, foo])
+      } else {
+        yellowData.push([this.props.usageData[i].slot, this.props.usageData[i].value])
+      }
+
   	}
 
 		$(function () {
 	    $('#highcharts').highcharts({
 				chart: {
-				  type: 'area',
 				  style: {
 				  	fontFamily: 'Avenir-Medium'
 					}
 				},
-				colors: ['#66c2a5'],
+				colors: ['#d53e4f', '#fee08b', '#3288bd'],
 				credits: {
 				    enabled: false
 				},
 				legend: {
-					enabled: true
+					enabled: false
 				},
+        plotOptions: {
+          series: {
+            marker: {
+              enabled: false
+            }
+          },
+          area: {
+            fillOpactiy: 1.0,
+            lineColor: '#3288bd'
+          }
+        },
 				series: [{
-				    name: 'Baseline Data',
-				    data: baselineData,
-				    negativeColor: '#d53e4f'
+          type: 'area',
+			    name: 'Green Data',
+          color: '#99d594',
+          fillOpactiy: 1.0,
+			    data: greenData,
+          index: 100
 				},
         {
-            name: 'Chart Data',
-            data: chartData,
-            negativeColor: '#d53e4f'
+          type: 'area',
+          name: 'Red Data',
+          color: '#d53e4f',
+          fillOpactiy: 1.0,
+          data: redData,
+          index: 10
+        },
+        {
+          type: 'area',
+          name: 'Yellow Data',
+          color: '#F7F7F7',
+          fillOpactiy: 1.0,
+          data: yellowData,
+          index: 1000
+        },
+        {
+          type: 'line',
+          name: 'Baseline',
+          color: '#666',
+          fillOpactiy: 1.0,
+          data: greenData,
+          index: 1000
         }]
 	    });
 		});
